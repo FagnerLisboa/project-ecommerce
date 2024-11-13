647796c805623ec6d7cf4657a899e35d81ccd90e
Fagner Lisboa <fnlisboa2016@gmail.com>
2024-11-13 10:14:17 -0300

    fist commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './home/navbar/navbar.component';
+import { SearchBarComponent } from './home/search-bar/search-bar.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './home/footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ProductsComponent } from './products/products.component';
+import { ProductDetailsComponent } from './products/product-details/product-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    SearchBarComponent,
+    ShoppingCartComponent,
+    ContactComponent,
+    FooterComponent,
+    HeaderComponent,
+    NotFoundComponent,
+    ProductsComponent,
+    ProductDetailsComponent
   ],
   imports: [
     BrowserModule,
